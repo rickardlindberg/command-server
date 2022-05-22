be3da66f11048aacc8cc83e2ab34af751bf7a9b9
Rickard Lindberg <rickard@rickardlindberg.me>
2022-05-22 09:40:26 +0200

    First seed to command server.

---
+++ command-server.py
@@ -0,0 +1,159 @@
+"""
+A command server runs a command upon startup and repeatedly when told so.
+
+A command client invokes the appropriate command server based on file path.
+
+Step 1: Start server in a directory
+
+    $ cd /foo
+    $ command-server echo hello
+    hello
+
+The server "registers" in the particular directory, runs the command, and waits
+for invocations.
+
+Step 2: Start a client in a subdirectory of a server
+
+    $ cd /foo/bar
+    $ command-server --invoke
+    ok
+
+The client finds the server registered in /foo and invokes it causing the
+server to run the command again.
+
+Step 3: Start a client in a directory without a server registered
+
+    $ cd /baz
+    $ command-server --invoke
+    no server registered
+
+If the server can listen for changes to files in its directory, it could invoke
+itself and remove the need for the client.
+"""
+
+import sys
+import subprocess
+
+class CommandServerApp:
+
+    """
+    >>> arguments = ProgramArguments.create_null(["echo", "hello"])
+    >>> process_factory = ProcessFactory.create_null()
+    >>> app = CommandServerApp(arguments, process_factory)
+
+    It runs the command upon startup:
+
+    >>> app.run()
+    >>> process_factory.get_last_spawned()
+    Process(['echo', 'hello'])
+
+    It runs the command when invoked:
+
+    It can be instantiated without arguments:
+
+    >>> _ = CommandServerApp()
+    """
+
+    def __init__(self, arguments=None, process_factory=None):
+        self.arguments = arguments or ProgramArguments()
+        self.process_factory = process_factory or ProcessFactory()
+
+    def run(self):
+        self.process_factory.spawn(self.arguments.get())
+
+class ProgramArguments:
+
+    """
+    Test null version:
+
+    >>> ProgramArguments.create_null(['echo', 'hello']).get()
+    ['echo', 'hello']
+
+    Test real version:
+
+    >>> ProgramArguments().get()
+    ['--test']
+    """
+
+    @staticmethod
+    def create_null(arguments):
+        return ProgramArguments(SysNull(arguments))
+
+    def __init__(self, sys=sys):
+        self.sys = sys
+
+    def get(self):
+        return self.sys.argv[1:]
+
+class ProcessFactory:
+
+    """
+    Test null version:
+
+    >>> factory = ProcessFactory.create_null()
+    >>> factory.get_last_spawned()
+    >>> p = factory.spawn(['echo', 'hello'])
+    >>> factory.get_last_spawned()
+    Process(['echo', 'hello'])
+    >>> p is factory.get_last_spawned()
+    True
+    >>> p.get_pid() is None
+    True
+
+    Test real version:
+
+    >>> factory = ProcessFactory()
+    >>> process = factory.spawn(['echo', 'hello'])
+    >>> isinstance(process.get_pid(), int)
+    True
+    """
+
+    @staticmethod
+    def create_null():
+        return ProcessFactory(NullSubprocess())
+
+    def __init__(self, subprocess=subprocess):
+        self.last_spwaned = None
+        self.subprocess = subprocess
+
+    def spawn(self, command):
+        self.last_spwaned = Process(command, self.subprocess.Popen(command))
+        return self.last_spwaned
+
+    def get_last_spawned(self):
+        return self.last_spwaned
+
+class NullSubprocess:
+
+    def Popen(self, command):
+        return NullPopen()
+
+class NullPopen:
+
+    def __init__(self):
+        self.pid = None
+
+class Process:
+
+    def __init__(self, command, popen):
+        self.command = command
+        self.popen = popen
+
+    def get_pid(self):
+        return self.popen.pid
+
+    def __repr__(self):
+        return f"Process({self.command})"
+
+class SysNull:
+
+    def __init__(self, arguments):
+        self.argv = [None]+arguments
+
+if __name__ == "__main__":
+    if sys.argv[1:] == ["--test"]:
+        import doctest
+        doctest.testmod()
+        print("ok")
+    else:
+        CommandServerApp().run()
